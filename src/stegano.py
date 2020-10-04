bad42e2e129c9267fe94629ba01db55af3e4caca
Vincent_C <13517103@std.stei.itb.ac.id>
2020-10-04 23:32:24 +0700

    add video stegano

--- src/stegano.py
+++ src/stegano.py
@@ -1,19 +1,16 @@
 from PIL import Image
 import numpy as np
-import os
-import random
-import time
-from math import log10, sqrt
+from math import log10, sqrt, ceil
+import os, random, time, cv2
+import skvideo.io
 
 # File Management
 def read_image(path):
     image = Image.open(path)
     image_array = np.array(image)
-    image_dim = image_array.shape
-    image_array = np.ravel(image_array)
     image.close()
 
-    return image_array, image_dim
+    return image_array
 
 def read_file(path):
     f = open(path, 'rb')
@@ -65,13 +62,70 @@ def binary_to_bit(content):
 
 
 
+def LSB_encrypt(image_array, prefix, content, key=0, reseed=False, random_pixel=False):
+    dim = image_array.shape
+    image_array = np.ravel(image_array)
+
+    # Initialize Random Pixel List
+    idx_list = []
+    for i in range(len(prefix), len(image_array)):
+        idx_list.append(i)
+    if reseed:
+        random.seed(key)
+    if key and random_pixel:
+        random.shuffle(idx_list)
+
+    # Insert Message Description
+    for i in range(len(prefix)):
+        image_array[i] = bit_insertion(image_array[i], int(prefix[i]))
+
+    # Insert Message
+    for i, num in enumerate(idx_list):
+        if i==len(content):
+            break
+        image_array[num] = bit_insertion(image_array[num], int(content[i]))
+
+    image_array = np.reshape(image_array, dim)
+
+    return image_array
+
+def LSB_decrypt(file_name_len, file_size, file_size_len, stego_array, key=0, reseed=False, random_pixel=False):
+    # Initialize acak Pixel List
+    idx_list = []
+    content = bytearray()
+    start = 1+file_name_len if file_name_len!=0 else 0
+    start += 1+file_size_len if file_size_len!=0 else 0
+    for i in range(start*8, len(stego_array)):
+        idx_list.append(i)
+    if reseed:
+        random.seed(key)
+    if key and random_pixel:
+        random.shuffle(idx_list)
+
+    # Prune Searching List
+    idx_list = idx_list[:file_size]
+
+    # Get Message Content
+    while len(idx_list)>0:
+        eight_idx = idx_list[:8]
+        stego_byte = []
+        for idx in eight_idx:
+            stego_byte.append(stego_array[idx])
+        stego_char = byte_extraction(stego_byte)
+        content += stego_char
+        idx_list = idx_list[8:]
+
+    return content
+
+
 
 # Steganography Main Class
 class Stegano():
     @staticmethod
     def LSB_encrypt_image(image_path, message_path, key=0):
         # Initialize
-        image_array, initial_dim = read_image(image_path)
+        image_array = read_image(image_path)
+        image_type = 'RGB' if len(image_array.shape)==3 else 'L'
         image_path = image_path.split('/')
         output_file = image_path[0] + '/stego/' + image_path[2]
 
@@ -88,35 +142,18 @@ class Stegano():
         else:
             prefix = ascii_to_bit(len(message_path)) + str_to_bit(message_path) + ascii_to_bit(int(len(int_to_bit(bits_len))/8)) + int_to_bit(bits_len)
 
-        # Initialize Random Pixel List
-        idx_list = []
-        for i in range(len(prefix), len(image_array)):
-            idx_list.append(i)
-        if key:
-            random.seed(key)
-            random.shuffle(idx_list)
-
-        # Insert Message Description
-        for i in range(len(prefix)):
-            image_array[i] = bit_insertion(image_array[i], int(prefix[i]))
-
-        # Insert Message
-        for i, num in enumerate(idx_list):
-            if i==bits_len:
-                break
-            image_array[num] = bit_insertion(image_array[num], int(bits[i]))
+        image_array = LSB_encrypt(image_array, prefix, bits, key, True, key)
         
         # Save Stego Image
-        image_array = np.reshape(image_array, initial_dim)
-
-        stego_image = Image.fromarray(image_array.astype('uint8'), 'RGB')
+        stego_image = Image.fromarray(image_array.astype('uint8'), image_type)
         stego_image.save(output_file)
         print('Stego File save as: ' + output_file)
 
     @staticmethod
     def LSB_decrypt_image(stego_path, key=0):
         # Initialize Image List and Content
-        stego_array, dim = read_image(stego_path)
+        stego_array = read_image(stego_path)
+        stego_array = np.ravel(stego_array)
         content = bytearray()
         acak = False
 
@@ -144,26 +181,7 @@ class Stegano():
             file_size += stego_char
         # print(file_size)
         
-        # Initialize acak Pixel List
-        idx_list = []
-        for i in range((1+file_name_len+1+file_size_len)*8, len(stego_array)):
-            idx_list.append(i)
-        if key and acak:
-            random.seed(key)
-            random.shuffle(idx_list)
-
-        # Prune Searching List
-        idx_list = idx_list[:file_size]
-
-        # Get Message Content
-        while len(idx_list)>0:
-            eight_idx = idx_list[:8]
-            stego_byte = []
-            for idx in eight_idx:
-                stego_byte.append(stego_array[idx])
-            stego_char = byte_extraction(stego_byte)
-            content += stego_char
-            idx_list = idx_list[8:]
+        content += LSB_decrypt(file_name_len, file_size, file_size_len, stego_array, key, True, acak)
         
         # Write Message
         f = open('test/message/' + file_name, 'wb')
@@ -180,7 +198,9 @@ class Stegano():
 
     @staticmethod
     def payload_containable_image_LSB(ori_path, message_path):
-        ori_array, ori_dim = read_image(ori_path)
+        ori_array = read_image(ori_path)
+        ori_dim = ori_array.shape
+        ori_array = np.ravel(ori_array)
         ori_size = 1
         for each in ori_dim:
             ori_size *= each
@@ -193,20 +213,154 @@ class Stegano():
         else:
             return False
 
+    @staticmethod
+    def LSB_encrypt_video(video_path, message_path,
+        key=0, random_frame=False, random_pixel=False
+        ):
+        frames = skvideo.io.vread(video_path)
+        # frames = []
+        # gen = skvideo.io.vreader(video_path)
+        # frames.append(next(gen))
+        # frames.append(next(gen))
+        # frames = np.array(frames)
+        initial_dim = frames.shape
 
+        video_path = video_path.split('/')
+        output_file = 'test/stego/' + video_path[-1]
+        
+        content = read_file(message_path)
+        message_path = message_path.split('/')
+        message_path = message_path[-1]
+
+        bits = binary_to_bit(content)
+        bits_len = len(bits)
+        message_code = len(message_path)
+        if random_frame:
+            message_code += 128
+        if random_pixel:
+            message_code += 64
+        prefix = ascii_to_bit(message_code) + str_to_bit(message_path) + ascii_to_bit(int(len(int_to_bit(bits_len))/8)) + int_to_bit(bits_len)
+        
+        frames[0] = LSB_encrypt(frames[0], prefix, '', key, False, False)
+        
+        frame_size = initial_dim[1] * initial_dim[2] * initial_dim[3]
+        reseed = True
+
+        # Initialize Random Pixel List
+        idx_list = []
+        for i in range(1, initial_dim[0]):
+            idx_list.append(i)
+        if random_frame:
+            random.seed(key)
+            random.shuffle(idx_list)
+
+        for i in range(ceil(bits_len/frame_size)):
+            current_bits = bits[i*frame_size:(i+1)*frame_size]
+            
+            frames[idx_list[i]] = LSB_encrypt(frames[idx_list[i]], '', current_bits, key, reseed, random_pixel)
+
+            reseed = False
+        
+        writer = skvideo.io.FFmpegWriter(output_file,
+            outputdict={
+                '-vcodec': 'ffv1',
+                '-crf': '0',
+                '-preset':'veryslow',
+                }
+            )
+
+        for i in range(len(frames)):
+            # print(np.ravel(frames[i])[:8])
+            writer.writeFrame(frames[i])
+
+        writer.close()
+        print('Stego Video save as: ' + output_file)
+
+    @staticmethod
+    def LSB_decrypt_video(stego_path, key):
+        # Initialize
+        frames = skvideo.io.vread(stego_path)
+        initial_dim = frames.shape
+
+        # print(np.ravel(frames[0])[:8])
+        # print(np.ravel(frames[1])[:8])
+        # return
+
+        content = bytearray()
+        random_frame, random_pixel = False, False
+
+        # Get Message File Name
+        current_frame_array = np.ravel(frames[0])
+        file_name_len = ord(byte_extraction(current_frame_array[:8]))
+        if file_name_len>=128:
+            file_name_len -= 128
+            random_frame = True
+        if file_name_len>=64:
+            file_name_len -= 64
+            random_pixel = True
+        file_name = ''
+        for i in range(file_name_len):
+            start = 8*(i+1)
+            stego_byte = current_frame_array[start:8+start]
+            stego_char = byte_extraction(stego_byte)
+            file_name += stego_char.decode('utf-8')
+        
+        # Get Message File Size
+        start = 8*(1+file_name_len)
+        file_size_len = ord(byte_extraction(current_frame_array[start:8+start]))
+        file_size = 0
+        for i in range(file_size_len):
+            file_size = file_size << 8
+            start = 8*(i+1+file_name_len+1)
+            stego_byte = current_frame_array[start:8+start]
+            stego_char = ord(byte_extraction(stego_byte))
+            file_size += stego_char
+        # print(file_size)
+
+        frame_size = initial_dim[1] * initial_dim[2] * initial_dim[3]
+        reseed = True
+
+        # Initialize Random Pixel List
+        idx_list = []
+        for i in range(1, initial_dim[0]):
+            idx_list.append(i)
+        if random_frame:
+            random.seed(key)
+            random.shuffle(idx_list)
+
+        for i in range(ceil(file_size/frame_size)):
+            current_frame_array = np.ravel(frames[idx_list[i]])
+            size = file_size-frame_size if file_size>frame_size else file_size
+            content += LSB_decrypt(0, size, 0, current_frame_array, key, reseed, random_pixel)
+            reseed = False
+            file_size = file_size-frame_size if file_size>frame_size else 0
+        
+        # Write Message
+        f = open('b.txt', 'w')
+        f.write(binary_to_bit(content))
+        f.close()
+        f = open('test/message/' + file_name, 'wb')
+        f.write(content)
+        f.close()
+        print('Message File Saved as ' + file_name)
 
 print("============================== Start ==============================")
 start_time = time.time()
 stego = Stegano()
 
-ori_path = 'test/ori/LogoITB.png'
 message_path = 'test/ori/tato.png'
-stego_path = 'test/stego/LogoITB.png'
+# ori_path = 'test/ori/LogoITB.png'
+# stego_path = 'test/stego/LogoITB.png'
+ori_path = 'test/ori/video.avi'
+stego_path = 'test/stego/video.avi'
 key = 50
 
 # if stego.payload_containable_image_LSB(ori_path, message_path):
 #     stego.LSB_encrypt_image(ori_path, message_path, key)
-stego.LSB_decrypt_image(stego_path, key)
+# stego.LSB_decrypt_image(stego_path, key)
+
+stego.LSB_encrypt_video(ori_path, message_path, key, True, True)
+stego.LSB_decrypt_video(stego_path, key)
 
 print("--- %s seconds ---" % (time.time() - start_time))
 print("============================== End ==============================")
